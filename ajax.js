89a3b2d46d561d8c68b518839c74bd1ba86cea3d
Zengyi20160602 <3054667507@qq.com>
2016-12-22 11:57:29 +0800

    Update ajax.js

--- ajax.js
+++ ajax.js
@@ -4,16 +4,13 @@ function ajaxGet(){
 	
 	http.onreadystatechange = function() {
 		if(http.readyState == 4) {
-			console.log("发送没有问题");
 			if (http.status == 200) {
-				
-				console.log("所以是有返回的");
 				var ajaxRes = document.getElementById("ajaxResponse");
 				ajaxRes.innerHTML = http.responseText;
 			}
 		}
 	}
-	http.open("get",url,true);
+	http.open("get",url);
 	http.send(null);
 }
 function ajaxClick() {
